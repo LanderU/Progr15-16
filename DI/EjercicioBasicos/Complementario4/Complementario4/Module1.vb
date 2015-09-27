e792167a7467314d4f91e5607e7c0dcb16fdb2fc
Lander <lander@MacBook-Pro-de-Lander.local>
2015-09-27 12:04:05 +0200

    Add Complementario4

---
+++ DI/EjercicioBasicos/Complementario4/Complementario4/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    Function comprobaciones(ByRef n1, ByRef n2) As String
+        Console.Write("Introduzca un número: ")
+        n1 = Double.Parse(Console.ReadLine())
+        Console.Write("Introduzca el segundo número: ")
+        n2 = Double.Parse(Console.ReadLine())
+        limpiar()
+        If n1 > n2 Then
+            Return "El número uno es mayor que el dos"
+        ElseIf n2 > n1
+            Return "El número dos es mayor que el uno"
+        Else
+            Return "Ambos son iguales"
+        End If
+    End Function
+    Sub pausar()
+        Threading.Thread.Sleep(2000)
+    End Sub
+    Sub limpiar()
+        Console.Clear()
+    End Sub
+
+    Sub Main()
+        ' Introducir dos números por teclado e imprimir un mensaje indicando si el número
+        ' es mayor, menor o igual que el número dos.
+        Dim n1, n2 As Double
+        Console.WriteLine(comprobaciones(n1, n2))
+        pausar()
+
+    End Sub
+
+
+
+
+End Module
