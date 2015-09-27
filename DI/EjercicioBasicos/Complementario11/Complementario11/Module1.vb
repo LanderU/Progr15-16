4b11a1c4a3188e9ce052db33e4765fdec4a25bd4
Lander <lander@MacBook-Pro-de-Lander.local>
2015-09-27 14:02:00 +0200

    Add Complementario11

---
+++ DI/EjercicioBasicos/Complementario11/Complementario11/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    Sub pausar()
+        Threading.Thread.Sleep(1000)
+    End Sub
+
+    Sub Main()
+        ' Visualiza todos los números del 1 al 1000 ambos incluidos.
+        Dim n As Integer = 1
+        Dim n2 As Integer = 100
+
+        For n = n To n2
+            Console.WriteLine(n)
+            pausar()
+
+        Next
+
+    End Sub
+
+End Module
