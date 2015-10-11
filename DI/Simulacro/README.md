a879f07ec7f420ddf00c895d2fa0e3e2e92222a7
Lander Usategui San Juan <lander@MacBook-Pro-de-Lander.local>
2015-10-11 10:37:19 +0200

    Add Simulacro and proyect 1

---
+++ DI/Simulacro/README.md
@@ -0,0 +1,3 @@
+# Simulacro de examen.
+
+1. Introducir a través del teclado un número entero positivo y distinto de 0. Posteriormente imprimir un mensaje indicando si ese número es par o impar.
---
+++ DI/Simulacro/Simulacro1/Simulacro1/Module1.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+    Private Sub pausar()
+        Threading.Thread.Sleep(2000)
+    End Sub
+    Private Sub clear()
+        Console.Clear()
+    End Sub
+    Sub Main()
+
+        ' Introducir a través del teclado un número entero positivo y distinto de 0.
+        ' Posteriormente imprimir un mensaje indicando si ese número es par o impar.
+
+        Dim n As Integer
+        Do
+            clear()
+            Console.Write("Introduzca un número entero positivo y distinto de 0: ")
+            Try
+                n = Integer.Parse(Console.ReadLine())
+            Catch ex As Exception
+                Console.WriteLine("Número no válido vuelva a intetntarlo.")
+                pausar()
+                n = 0
+            End Try
+
+        Loop While n <= 0
+        clear()
+        If n Mod 2 = 0 Then
+            Console.WriteLine("{0}, es un número par.", n)
+            pausar()
+        Else
+            Console.WriteLine("{0}, es un número impar", n)
+            pausar()
+        End If
+
+
+    End Sub
+
+End Module
