a2b11e9d94262116942344f2556d248a6165d228
LanderU <lander.usategui@gmail.com>
2015-10-14 23:01:12 +0200

    Update README.md and first proyect of EjercicioEstructuras2

---
+++ DI/Estructuras/EjercicioEstructuras2/EjercicioEstructura2/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+    Private Sub pausar()
+        Threading.Thread.Sleep(2000)
+    End Sub
+    Private Sub clear()
+        Console.Clear()
+    End Sub
+
+    Structure asignaturas
+        Dim nomAsig As String
+        Dim nota As Integer
+    End Structure
+
+    Sub Main()
+
+        ' Desarrollar un programa que permita guardar las notas de
+        ' los alumnos.El programa debe ir pidiendo el nombre del 
+        ' alumno, las asignaturas de las que está matriculado y la 
+        ' nota obtenida en cada una de ellas. Hay que tener en 
+        ' cuenta que hay seis posibles asignaturas, pero no todos
+        ' los alumnos están matriculados de todas.
+        ' Creamos el array alumnos
+        Dim alumnos() As String
+        ' Array de las asignaturas de las cuales elegiremos
+        Dim posiblesAsignaturas() As String = {"VB", "JAVA", "SO", "ENDE", "PDM"}
+        ' Array escalonada en la que guardaremos en la primera dimensión el alumno y en la segunda las asignaturas y sus notas
+        Dim matriculaAlumnos()() As asignaturas
+
+        Console.Write("Nombre del alumno: ")
+        alumnos(0) = Console.ReadLine()
+
+        Console.Write("Seleccione el número de notas: ")
+        Dim numAsignaturas As Integer = Console.ReadLine()
+
+        matriculaAlumnos(0) = New asignaturas(numAsignaturas) {}
+        matriculaAlumnos(0)(0).nomAsig = "VB"
+        matriculaAlumnos(0)(0).nota = 5
+
+
+
+
+    End Sub
+
+End Module
--- DI/Estructuras/README.md
+++ DI/Estructuras/README.md
@@ -1,3 +1,4 @@
 # Ejercicios relacionados con las estructuras de datos
 
 1. Crea un arrayList con los nombres de 10 alumnos de clase.Muéstralos ordenados alfabéticamente de forma ascendente.Muéstralos en forma descendente. Inserta en la primera posición una etiqueta con el código del curso y el nombre del tutor. Busca dentro del arrayList un nombre que te introducirán desde el teclado y le indicaras si está o no en la lista.
+2. Desarrollar un programa que permita guardar las notas de los alumnos.El programa debe ir pidiendo el nombre del alumno, las asignaturas de las que está matriculado y la nota obtenida en cada una de ellas. Hay que tener en cuenta que hay seis posibles asignaturas, pero no todos los alumnos están matriculados de todas.
