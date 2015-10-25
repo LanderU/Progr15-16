2f000c9dd981869b774714039da7a709f47765af
LanderU <lander.usategui@gmail.com>
2015-10-25 12:22:57 +0100

    Add next entregas

---
+++ DI/Entregas/Entrega4LanderUsategui/Entrega4LanderUsategui/Module1.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+    Private Sub clear()
+        Console.Clear()
+    End Sub
+    Private Sub pausar()
+        Threading.Thread.Sleep(2000)
+    End Sub
+
+
+    Sub Main()
+        ' Crea    una	matriz	con	los	nombre	de	todos	los	alumnos	de	clase.	Se	pide	el
+        ' número  de	alumnos	de	clases	y	sus	nombres.	Podremos	interrumpir	la	entrada	
+        ' de   datos	 pulsando	 [Ctrl][Z] ([Ctrl][Z] =	 nothing).	 Se	 preguntará	 si	 se	 desea	
+        ' visualizar  la	lista	completa, en	cualquier	caso	se	podrá	introducir	un	número	y
+        ' nos devolverá el	nombre	del	alumnos	que	se	tecleó	en	esa	posición.
+
+        Dim alumnos() As String = {}
+
+        Dim numAlumnos As Integer
+
+        Dim tecla As ConsoleKeyInfo
+
+        Do
+            clear()
+            Try
+                Console.Write("Escriba la cantidad de alumnos a matricular: ")
+                numAlumnos = Integer.Parse(Console.ReadLine()) - 1
+                If numAlumnos < 0 Then
+                    Console.WriteLine("Vuelve a intentarlo.")
+                    pausar()
+                End If
+            Catch ex As Exception
+                Console.WriteLine("Dato no válido.")
+                numAlumnos = Integer.MinValue
+                pausar()
+            End Try
+
+        Loop While numAlumnos < 0
+
+        ReDim alumnos(numAlumnos)
+        clear()
+        For i As Integer = 0 To numAlumnos
+            Console.Write("Introduzca el nombre del alumno: ")
+            alumnos(i) = Console.ReadLine()
+        Next
+
+        For i As Integer = 0 To numAlumnos
+            Console.WriteLine(alumnos(i))
+            pausar()
+        Next
+
+
+    End Sub
+
+End Module
---
+++ DI/Entregas/Entrega5LanderUsategui/Entrega5LanderUsategui/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+    Private Sub pausar()
+        Threading.Thread.Sleep(2000)
+    End Sub
+    Private Sub clear()
+        Console.Clear()
+    End Sub
+
+
+    Sub Main()
+        'Realiza    un	 programa	 que	 lea	 una	 lista	 de	 valores	 introducidos por	 el
+        'teclado. A   continuación, y	 sobre	 la	 lista, se	 tienen que	 buscar	 los	 valores	
+        'máximo  y	mínimo	y	mostrarlos	por	pantalla
+
+        Dim lista As New ArrayList
+        Dim continuar As String = "s"
+
+        Do
+            clear()
+            Console.Write("Introduzca el valor en la lista: ")
+            lista.Add(Console.ReadLine())
+            Console.Write("Añadir más valores a la lista s/n: ")
+            continuar = Console.ReadLine().ToLower
+        Loop While continuar <> "n"
+
+        lista.Sort()
+        Console.WriteLine(lista(0))
+        pausar()
+        pausar()
+        lista.Reverse()
+        Console.WriteLine(lista(0))
+        pausar()
+        pausar()
+    End Sub
+
+End Module
