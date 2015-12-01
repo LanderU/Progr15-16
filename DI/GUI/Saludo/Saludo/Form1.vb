d2fa06cdc273ece1555b416d0bd32ee52e73b686
LanderU <lander.usategui@gmail.com>
2015-12-01 19:48:56 +0100

    Add first GUI exercise

---
+++ DI/GUI/Saludo/Saludo/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked Then
+            TextBox1.Text = "Hola!"
+        ElseIf RadioButton2.Checked Then
+            TextBox1.Text = "Kaixo!"
+        End If
+    End Sub
+End Class
