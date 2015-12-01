1bb0d1da146e99a9fc678ffccebf093582a551ae
LanderU <lander.usategui@gmail.com>
2015-12-01 20:10:48 +0100

    Add template of tres en raya

---
+++ DI/GUI/TresenRaya/TresenRaya/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        'Salimos del programa
+        MsgBox("Gracias por jugar al tres en raya.")
+        Close()
+    End Sub
+End Class
