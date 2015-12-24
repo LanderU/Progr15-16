873efe4f78fce0826ea643a738b6c4b8d628fc42
LanderU <lander.usategui@gmail.com>
2015-12-24 20:59:39 +0100

    Add all buttons

--- DI/GUI/Buscaminas1.0/Buscaminas1.0/Form1.vb
+++ DI/GUI/Buscaminas1.0/Buscaminas1.0/Form1.vb
@@ -4,8 +4,8 @@
         Button37.Enabled = False
     End Sub
     ' Método que recoge el pulsado de todos los botones.
-    Private Sub pulsacion(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click
-        ' Hacemos el cast del botón un una variable
+    Private Sub pulsacion(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click, Button10.Click, Button11.Click, Button12.Click, Button13.Click, Button14.Click, Button15.Click, Button16.Click, Button17.Click, Button18.Click, Button19.Click, Button20.Click, Button21.Click, Button22.Click, Button23.Click, Button24.Click, Button25.Click, Button26.Click, Button27.Click, Button28.Click, Button29.Click, Button30.Click, Button31.Click, Button32.Click, Button33.Click, Button34.Click, Button35.Click, Button36.Click
+        ' Hacemos el cast del botón un una variable, no es necesario lo hace por si mismo, es por pulcritud mental
         Dim boton_clicado As Button = CType(sender, Button)
         accion_boton(boton_clicado)
     End Sub
