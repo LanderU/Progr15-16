4882e5c7f957e6cc5f5062bd59f33d68b1304763
LanderU <lander.usategui@gmail.com>
2015-12-25 14:28:15 +0100

    Add array

--- DI/GUI/Buscaminas1.0/Buscaminas1.0/Form1.vb
+++ DI/GUI/Buscaminas1.0/Buscaminas1.0/Form1.vb
@@ -1,43 +1,10 @@
 ﻿Public Class Form1
-    ' Definición del tablero de juego 3 = Bomba
-    Private Sub tablero()
-        Button1.Text = "3"
-        Button2.Text = "1"
-        Button3.Text = "0"
-        Button4.Text = "0"
-        Button5.Text = "0"
-        Button6.Text = "0"
-        Button7.Text = "1"
-        Button8.Text = "0"
-        Button9.Text = "1"
-        Button10.Text = "0"
-        Button11.Text = "0"
-        Button12.Text = "0"
-        Button13.Text = "0"
-        Button14.Text = "1"
-        Button15.Text = "3"
-        Button16.Text = "1"
-        Button17.Text = "1"
-        Button18.Text = "0"
-        Button19.Text = "0"
-        Button20.Text = "0"
-        Button21.Text = "1"
-        Button22.Text = "1"
-        Button23.Text = "3"
-        Button24.Text = "1"
-        Button25.Text = "1"
-        Button26.Text = "0"
-        Button27.Text = "0"
-        Button28.Text = "1"
-        Button29.Text = "1"
-        Button30.Text = "0"
-        Button31.Text = "3"
-        Button32.Text = "1"
-        Button33.Text = "1"
-        Button34.Text = "3"
-        Button35.Text = "1"
-        Button36.Text = "0"
-    End Sub
+    ' Array con el tablero 3 = Bomba
+    Dim tablero = {
+                        {3, 1, 0, 0, 0, 0},
+                        {1, 0, 1, 0, 0, 0}
+                   }
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Label2.Visible = False
         Button37.Enabled = False
