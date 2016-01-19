3d94f57d9004d10070c9bb7a2ed4a9bee5054a2e
LanderU <lander.usategui@gmail.com>
2016-01-19 19:10:20 +0100

    Proyecto gestionar eventos

---
+++ DI/GUI/CapturarEventos/CapturarEventos/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Cerramos la aplicación")
+        Close()
+    End Sub
+
+    Private Sub ponerseEncimaBotonSalir(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        Button1.BackColor = Color.Cyan
+    End Sub
+
+    Private Sub estarFueraBotonSalir(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+
+        Button1.ResetBackColor()
+
+    End Sub
+
+    Private Sub clicarAbajo(sender As Object, e As EventArgs) Handles Button2.MouseEnter
+        AddHandler Button2.Click, AddressOf funcionLlamada
+    End Sub
+
+    Private Sub funcionLlamada(sender As Object, e As EventArgs)
+        Text = "Prueba"
+    End Sub
+End Class
