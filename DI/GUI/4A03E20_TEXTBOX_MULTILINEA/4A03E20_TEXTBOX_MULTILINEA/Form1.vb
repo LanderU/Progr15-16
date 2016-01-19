dcccb2c573a9a8d18ccdbffe26285e0ed1d1ca14
LanderU <lander.usategui@gmail.com>
2016-01-19 20:07:47 +0100

    Ejercicio multilinea

---
+++ DI/GUI/4A03E20_TEXTBOX_MULTILINEA/4A03E20_TEXTBOX_MULTILINEA/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Añadimos las etiquetas
+        Label1.Text = "EDITOR MULTILINEA"
+        Label2.Text = "Palabra"
+        Label7.Text = "Texto"
+        Label3.Text = "Caracteres en el control"
+        Label5.Text = "Caracteres seleccionados"
+        Label4.Text = "0"
+        Label6.Text = "0"
+        Button1.Text = "Aceptar"
+        Button2.Text = "Limpiar"
+        Button3.Text = "Limpiar"
+        Button4.Text = "Salir"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.AppendText(" " + TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox2.Clear()
+    End Sub
+
+    Private Sub contarTextBox1(sender As Object, e As EventArgs) Handles Me.MouseMove
+        Label6.Text = TextBox1.SelectedText.Count()
+
+    End Sub
+
+    Private Sub contarCajaCompleta(sender As Object, e As EventArgs) Handles Me.MouseMove
+        If (TextBox2.SelectedText.Count = 0) Then
+            Label4.Text = "0"
+        Else
+            Label4.Text = TextBox2.SelectedText.Count() - 1
+
+        End If
+    End Sub
+End Class
---
+++ DI/GUI/4A03E20_TEXTBOX_MULTILINEA/4A03E20_TEXTBOX_MULTILINEA/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("4A03E20_TEXTBOX_MULTILINEA")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("4A03E20_TEXTBOX_MULTILINEA")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c362ed7d-df23-46d5-a6a8-d523949488ca")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
