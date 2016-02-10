aeeb9a9a459ac0f80b43e268b4d8ed754c448d78
LanderU <lander.usategui@gmail.com>
2016-02-10 19:05:14 +0100

    Ejercicio InputBox

---
+++ DI/GUI/PruebaInputBox/PruebaInputBox/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Prueba InputBox"
+        Label1.Text = "DATOS"
+        Label2.Text = "Nombre: "
+        Label3.Text = "Apellido: "
+        Label4.Text = "Edad: "
+        Button1.Text = "Salir"
+        Label5.Text = ""
+        Label6.Text = ""
+        Label7.Text = ""
+
+        Dim nombre As String
+        Dim apellido As String
+        Dim edad As Integer
+
+        Dim input1 = InputBox(nombre, "Introduzca su nombre")
+        Dim input2 = InputBox(apellido, "Introduzca su apellido")
+        Dim input3 = InputBox(edad, "Introduzca su edad")
+
+        If input1.Count <> 0 Then
+            Label5.Text = input1.ToString
+        End If
+        If input2.Count <> 0 Then
+            Label6.Text = input2.ToString
+        End If
+        If input3.Count <> 0 Then
+            Label7.Text = input3.ToString
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+End Class
---
+++ DI/GUI/PruebaInputBox/PruebaInputBox/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PruebaInputBox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("PruebaInputBox")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6a592db9-f945-4a17-9d9e-c0ea8616df6d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
