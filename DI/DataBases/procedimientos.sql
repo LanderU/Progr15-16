43ab1635ad4956b5daa3b77f6d25ecb8118212a3
LanderU <lander.usategui@gmail.com>
2016-04-05 20:10:39 +0200

    Script para crear procedimientos

---
+++ DI/DataBases/procedimientos.sql
@@ -0,0 +1,64 @@
+
+/* Modificamos la tabla para que el campo sea único */
+/*ALTER TABLE PRODUCTOS add constraint campo_unico UNIQUE (cod_prod)*/
+
+/*
+	PROCEDIMIENTOS ALMACENADOS
+*/
+
+/* PROCEDIMIENTO MOSTRAR PRODUCTO */
+
+CREATE PROCEDURE MostrarProducto
+@cod_prod varchar(4)
+AS
+BEGIN
+	SELECT NOMBRE, EXISTENCIA
+	FROM productos
+	WHERE cod_prod = @cod_prod
+END
+GO
+/* PROCEDIMIENTO REGISTRAR PRODUCTO */
+
+CREATE PROCEDURE RegistrarProductos
+@cod_prod as varchar(4),
+@nombre as varchar(50),
+@existencia as int
+AS
+BEGIN
+	INSERT INTO productos (cod_prod,nombre,existencia) values (@cod_prod,@nombre,@existencia)
+END
+GO
+/* PROCEDIMIENTO MODIFICAR PRODUCTO */
+
+CREATE PROCEDURE ModificarProducto
+@cod_prod as varchar(4),
+@nombre as varchar(50),
+@existencia as int
+AS
+BEGIN
+	UPDATE productos SET nombre = @nombre, existencia = @existencia
+	WHERE cod_prod = @cod_prod
+END
+GO
+
+/* PROCEDIMIENTO ELIMINAR PRODUCTO */
+
+CREATE PROCEDURE EliminarProducto
+@cod_prod as varchar(4)
+AS
+BEGIN
+	DELETE
+	FROM productos
+	WHERE cod_prod = @cod_prod
+END
+GO
+
+/* PROCEDIMIENTO SUMAR EXISTENCIA */
+
+CREATE PROCEDURE SumarExistencia
+@cod_prod as varchar(4),
+@existencia as int
+AS
+BEGIN
+	UPDATE productos SET existencia = @existencia WHERE cod_prod = @cod_prod
+END
